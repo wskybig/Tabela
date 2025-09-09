c1d0a0598f79d954a728f8079a31d75f2458ae9f
wskybig <j303.re@gmail.com>
2025-09-09 21:14:25 +0200

    Add TestComponent.jsx sample React component

---
+++ src/TestComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TestComponent() {
+  return (
+    <div style={{ padding: 16, border: "1px dashed #999" }}>
+      <h3>TestComponent</h3>
+      <p>This is a sample React component for testing.</p>
+    </div>
+  );
+}
+
