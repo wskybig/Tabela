54977de0f31e9f3680ca2f3fcc95c53ffedb7107
wskybig <j303.re@gmail.com>
2025-09-10 15:52:16 +0200

    Remove dummyData import and usage from Wiadomosci component

--- src/Wiadomosci.jsx
+++ src/Wiadomosci.jsx
@@ -1,32 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import dummyData from './dummyData';
+import React from 'react';
 
 const Wiadomosci = () => {
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    // co 1 sekundę aktualizuj indeks, żeby zmienić wyświetlany element
-    const id = setInterval(() => {
-      setIndex((i) => (i + 1) % dummyData.length);
-    }, 1000);
-
-    return () => clearInterval(id);
-  }, []);
-
+  // Usunięto import i użycie dummyData. Komponent wyświetla teraz informację,
+  // że brak źródła danych lub należy podać realne dane.
   return (
     <div>
       <h2>Wiadomości</h2>
-      <ul>
-        {dummyData.map((item, idx) => (
-          <li key={idx} style={{ display: idx === index ? 'block' : 'none' }}>
-            <strong>{item['Imię']} {item['Nazwisko']}</strong>
-            <div>Temat: {item['Temat']}</div>
-            <div>Wiadomość: {item['Wiadomość']}</div>
-            <div>Status: {item['Status']}</div>
-            <div>Data wysłania: {item['Data wysłania']}</div>
-          </li>
-        ))}
-      </ul>
+      <p>Brak danych testowych. Podłącz źródło danych, aby wyświetlić wiadomości.</p>
     </div>
   );
 };
