393059fc91ddeb7c4276459f607e884f9544e954
wskybig <j303.re@gmail.com>
2025-09-10 13:46:48 +0200

    Render Wiadomosci on main page with test header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 ﻿import React from 'react'
 import MyForm from './MyForm.jsx'
+import Wiadomosci from './Wiadomosci'
 import './styles.css'
 
 export default function App() {
-  return <MyForm />
+  return (
+    <div>
+      <MyForm />
+      <hr />
+      <h2>DANE TESTOWE (dummy) – REFRESH 1s</h2>
+      <Wiadomosci />
+    </div>
+  )
 }
